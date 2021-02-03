4dac1bc5ac0177b2ebedcbc06c2a3d2d86561e8f
The Rite Sites <parker@theritesites.com>
2021-02-03 16:26:43 -0700

    updated packages for security

--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,12 @@ const CopyWebpackPlugin = require( 'copy-webpack-plugin' );
 const browserSyncPlugin = require( 'browser-sync-webpack-plugin' );
 const WebpackZipPlugin = require('webpack-zip-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { env } = require('process');
 
 const pluginSlug = 'order-simulator-woocommerce';
 
 const buildFolder = path.resolve( __dirname, pluginSlug );
 // const vendorFolder = path.resolve( buildFolder, 'vendor' );
-
-// const devFolder = '/Users/parker/sites/wordpress/wp-content/plugins/' + pluginSlug;
-// const endPath = '/Users/parker/Documents/theritesites/completed_plugins';
-
-
-const devFolder = '/var/www/wpdev.com/public_html/wp-content/plugins/' + pluginSlug; // Corsair
-const endPath = '/home/parkerm34/Documents/theritesites/completed_plugins'; // Corsair
-
-const endFolder = endPath + '/' + pluginSlug;
 
 
 const config = env => {
@@ -24,23 +16,44 @@ const config = env => {
     const pluginList = [];
     console.log(env);
 
+
+    let devFolder = '';
+    let endPath = '';
+
+    if ( env.LOC === "m" ) {
+        devFolder = '/Users/parker/sites/wordpress/wp-content/plugins/' + pluginSlug;
+        endPath = '/Users/parker/Documents/theritesites/completed_plugins';
+    }
+    if ( env.LOC === "c" ) {
+        devFolder = '/var/www/wpdev.com/public_html/wp-content/plugins/' + pluginSlug; // Corsair
+        endPath = '/home/parkerm34/Documents/theritesites/completed_plugins'; // Corsair
+    }
+    if ( env.LOC === "m1" ) {
+        devFolder = '/Users/parkermathewson/mac-sites/wp56tester/wp-content/plugins/' + pluginSlug; // M1
+        endPath = '/Users/parkermathewson/Library/Mobile\ Documents/com~apple~CloudDocs/theritesites/completed_pluginsv2'; // M1
+        buildPath = '/Users/parkermathewson/theritesites/completed_pluginsv2'; // M1
+    }
+
+    const endFolder = endPath + '/' + pluginSlug;
+
+
+
     if(env === 'production' ) {
         pluginList.push(
-            new CopyWebpackPlugin( [
-                { from: path.resolve( __dirname, 'README.*' ), to: buildFolder },
-                { from: path.resolve( __dirname, 'fakenames.sql' ), to: buildFolder },
-                { from: path.resolve( __dirname, '*.php' ), to: buildFolder },
-                /** Above is for zip folder. Below is for repositories. **/
-                { from: path.resolve( __dirname, 'README.*' ), to: endFolder },
-                { from: path.resolve( __dirname, 'fakenames.sql' ), to: endFolder },
-                { from: path.resolve( __dirname, '*.php' ), to: endFolder },
-            ], {
-
-                copyUnmodified: true
+            new CopyWebpackPlugin( {
+                patterns: [
+                    { from: path.resolve( __dirname, 'README.*' ), to: buildFolder },
+                    { from: path.resolve( __dirname, 'fakenames.sql' ), to: buildFolder },
+                    { from: path.resolve( __dirname, '*.php' ), to: buildFolder },
+                    /** Above is for zip folder. Below is for repositories. **/
+                    { from: path.resolve( __dirname, 'README.*' ), to: endFolder },
+                    { from: path.resolve( __dirname, 'fakenames.sql' ), to: endFolder },
+                    { from: path.resolve( __dirname, '*.php' ), to: endFolder },
+                ]
             } ),
-            new WebpackZipPlugin({
+            new WebpackZipPlugin( {
                 initialFile: pluginSlug,
-                endPath: endPath,
+                endPath: buildPath,
                 zipName: pluginSlug + '.zip'
             } )
         );
@@ -63,13 +76,12 @@ const config = env => {
                 ],
                 reloadDelay: 0
             }),
-            new CopyWebpackPlugin( [
-                { from: path.resolve( __dirname, 'README.*' ), to: devFolder },
-                { from: path.resolve( __dirname, 'fakenames.sql' ), to: devFolder },
-                { from: path.resolve( __dirname, '*.php' ), to: devFolder }
-            ], {
-
-                copyUnmodified: false
+            new CopyWebpackPlugin( {
+                patterns: [
+                    { from: path.resolve( __dirname, 'README.*' ), to: devFolder },
+                    { from: path.resolve( __dirname, 'fakenames.sql' ), to: devFolder },
+                    { from: path.resolve( __dirname, '*.php' ), to: devFolder }
+                ]
             } ),
         );
     }
